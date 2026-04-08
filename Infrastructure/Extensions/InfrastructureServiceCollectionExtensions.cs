12c456c22d026317cc36788897c65ef88573ad40
crL96 <190506813+crL96@users.noreply.github.com>
2026-04-08 11:47:36 +0200

    Add infrastructure and application service collection extensions and install packages

---
+++ Infrastructure/Extensions/InfrastructureServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Infrastructure.Extensions;
+
+public static class InfrastructureServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration, IHostEnvironment env)
+    {
+        ArgumentNullException.ThrowIfNull(configuration);
+        ArgumentNullException.ThrowIfNull(env);
+
+        services.AddPersistence(configuration, env);
+
+        return services;
+    }
+}
