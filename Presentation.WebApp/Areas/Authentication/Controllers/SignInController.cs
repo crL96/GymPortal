9e60be0d01e2d79671aac6a9f8ef820956a53499
crL96 <190506813+crL96@users.noreply.github.com>
2026-04-10 15:10:04 +0200

    Add handling of sign-in post, signing in user

--- Presentation.WebApp/Areas/Authentication/Controllers/SignInController.cs
+++ Presentation.WebApp/Areas/Authentication/Controllers/SignInController.cs
@@ -1,14 +1,58 @@
+using Application.Abstractions.Auth;
+using Application.Dtos.Auth;
 using Microsoft.AspNetCore.Mvc;
+using Presentation.WebApp.Areas.Authentication.Models;
 
 namespace Presentation.WebApp.Areas.Authentication.Controllers;
 
 [Area("Authentication")]
 [Route("sign-in")]
-public class SignInController() : Controller
+public class SignInController(IAuthService authService) : Controller
 {
     public IActionResult Index(string? returnUrl = null)
     {
         ViewBag.ReturnUrl = returnUrl;
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(SignInForm form, string? returnUrl = null)
+    {
+        ViewBag.ReturnUrl = returnUrl;
+        if (!ModelState.IsValid)
+            return View(form);
+
+        try
+        {
+            var signInResult = await authService.SignInUserAsync(form.Email, form.Password, form.RememberMe);
+
+            if (!signInResult.Succeeded)
+            {
+                var errorMessage = signInResult.ErrorType switch
+                {
+                    AuthErrorType.RequiresTwoFactorAuth => "This user requires two-factor authentication",
+                    AuthErrorType.IsLockedOut => "This user is locked out. Please contact support.",
+                    AuthErrorType.NotAllowed => "You are not allowed to login. Please contact support.",
+                    _ => "Incorrect email address or password"
+                };
+
+                ModelState.AddModelError(nameof(form.ErrorMessage), errorMessage);
+                ViewBag.ReturnUrl = returnUrl;
+
+                return View(form);
+            }
+
+            if (!string.IsNullOrWhiteSpace(returnUrl))
+                return Redirect(returnUrl);
+
+            return Redirect("/");
+        }
+        catch
+        {
+            ModelState.AddModelError(nameof(form.ErrorMessage), "Something went wrong, please try again");
+            ViewBag.ReturnUrl = returnUrl;
+
+            return View(form);
+        }
     }
 }
