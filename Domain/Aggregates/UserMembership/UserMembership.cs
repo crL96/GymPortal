06d5c4193592b624a49c12cd88b4caad107f59ff
crL96 <190506813+crL96@users.noreply.github.com>
2026-04-15 16:00:43 +0200

    Add UserMembership domain aggregate

---
+++ Domain/Aggregates/UserMembership/UserMembership.cs
@@ -0,0 +1,50 @@
+using System;
+using Domain.Aggregates.Membership.ValueObjects;
+using Domain.Common.Exceptions;
+
+namespace Domain.Aggregates.UserMembership;
+
+public class UserMembership
+{
+    public string UserId { get; set; }
+    public MembershipId MembershipId { get; set; }
+    public bool IsActive { get; set; }
+
+    private UserMembership(string userId, MembershipId membershipId, bool isActive)
+    {
+        UserId = userId;
+        MembershipId = membershipId;
+        IsActive = isActive;
+    }
+
+    public static UserMembership Create(string userId, MembershipId membershipId)
+    {
+        if (string.IsNullOrWhiteSpace(userId))
+            throw new InvalidIdDomainException("UserId cannot be null or empty");
+
+        return new(userId, membershipId, true);
+    }
+
+    public static UserMembership Recreate(string userId, MembershipId membershipId, bool isActive)
+    {
+        if (string.IsNullOrWhiteSpace(userId))
+            throw new InvalidIdDomainException("UserId cannot be null or empty");
+
+        return new(userId, membershipId, isActive);
+    }
+
+    public void ChangePlan(MembershipId newMembershipId)
+    {
+        MembershipId = newMembershipId;
+    }
+
+    public void CancelPlan()
+    {
+        IsActive = false;
+    }
+
+    public void ReactivatePlan()
+    {
+        IsActive = true;
+    }
+}
