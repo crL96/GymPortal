07609a9ae1e85900748a32f7d383ab192ef6c8ed
crL96 <190506813+crL96@users.noreply.github.com>
2026-04-18 17:50:55 +0200

    Add hamburger menu functionality and improve responsiveness

---
+++ Presentation.WebApp/wwwroot/js/menu.js
@@ -0,0 +1,4 @@
+document.querySelector("#mobile-menu-button").addEventListener("click", (e) => {
+    const headerSection = document.querySelector("#header-section");
+    headerSection.classList.toggle("active");
+});
