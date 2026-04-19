2e05dcebcc248850db3c50dbc28acdbc2c847daf
crL96 <190506813+crL96@users.noreply.github.com>
2026-04-19 09:38:34 +0200

    Add domain Membership and MembershipId unit tests

---
+++ Tests/Domain/Memberships/MembershipIdTests.cs
@@ -0,0 +1,54 @@
+using Domain.Aggregates.Membership.ValueObjects;
+using Domain.Common.Exceptions;
+
+namespace Tests.Domain.Memberships;
+
+public class MembershipIdTests
+{
+
+    [Fact]
+    public void Create_Should_Return_Valid_MembershipId()
+    {
+        // Act
+        var id = MembershipId.Create();
+
+        // Assert
+        Assert.IsType<Guid>(id.Value);
+        Assert.NotEqual(Guid.Empty, id.Value);
+    }
+
+    [Fact]
+    public void Create_Should_Return_Unique_Ids()
+    {
+        // Act
+        var id1 = MembershipId.Create();
+        var id2 = MembershipId.Create();
+
+        // Assert
+        Assert.NotEqual(id1.Value, id2.Value);
+    }
+
+    [Fact]
+    public void Recreate_Should_Return_Id_When_Guid_Is_Valid()
+    {
+        // Arrange
+        var guid = Guid.NewGuid();
+
+        // Act
+        var id = MembershipId.Recreate(guid);
+
+        // Assert
+        Assert.Equal(guid, id.Value);
+    }
+
+    [Fact]
+    public void Recreate_Should_Throw_When_Guid_Is_Empty()
+    {
+        // Arrange
+        var guid = Guid.Empty;
+
+        // Act & Assert
+        Assert.Throws<InvalidIdDomainException>(() =>
+            MembershipId.Recreate(guid));
+    }
+}
